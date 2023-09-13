49ede104e915030bd3bc1352e455e5b618ae29a6
Nhan987 <132928751+Nhan987@users.noreply.github.com>
2023-09-13 14:40:47 +0700

    Committing local changes to AccountRepository.java

--- 001_Signin_and_login/src/main/java/com/example/Signin_and_login/repository/AccountRepository.java
+++ 001_Signin_and_login/src/main/java/com/example/Signin_and_login/repository/AccountRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
-
-    @Query()
+//ok
+    
 }
